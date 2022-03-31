faa36833e6ce534cf9e8a484ced1ff90dc167521
Adib128 <aouadi.adib@gmail.com>
2022-03-31 10:57:14 +0100

    Issue model, migration, queries and mutations added

---
+++ database/migrations/2022_03_31_084552_create_issues_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateIssuesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('issues', function (Blueprint $table) {
+            $table->id();
+            $table->integer('reference');
+            $table->dateTime('issueDate');
+            $table->text('summary');
+            $table->longText('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('issues');
+    }
+}
